381c1d10187429495aa0a3bc1fd6be3e7329d9fb
Percy Maldonado <maldonadoq.percy@gmail.com>
2022-04-11 12:46:17 -0300

    encoded and decoded images

---
+++ exercise/ex1/decode.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2 as cv
+import argparse
+
+
+def steganography_decode(input_image, bit_plane):
+    # Get k Least Significant Bit
+    mask = 1 << bit_plane
+    #input_bits = (input_image & mask).ravel() // bit_plane
+    input_bits = (input_image & mask).ravel() // mask
+
+    # Broadcast to array nx8 Dim
+    output_bits = input_bits.reshape((-1, 8))
+    output_bytes = np.sum(output_bits * [128, 64, 32, 16, 8, 4, 2, 1], axis=1)
+
+    output_text = ''
+    for ascii in output_bytes:
+        if ascii == 0:
+            break
+        output_text += chr(ascii)
+
+    return output_text
+
+
+def main():
+
+    # Add arguments to encode steganography program
+    parser = argparse.ArgumentParser(description='Steganography Decoder.')
+    parser.add_argument('input_image', help='Name of the encoded image.')
+    parser.add_argument('output_text', help='Name of the decoded text.')
+    parser.add_argument('bit_plane', help='Bit plane to find msg.', type=int)
+    args = parser.parse_args()
+
+    # Read encoded image file
+    input_image = cv.imread(args.input_image, cv.IMREAD_COLOR)
+
+    output_text = steganography_decode(input_image, args.bit_plane)
+    with open(args.output_text, 'w') as file:
+        file.write(output_text)
+
+
+if __name__ == "__main__":
+    main()
--- exercise/ex1/encode.py
+++ exercise/ex1/encode.py
@@ -2,8 +2,56 @@ import numpy as np
 import cv2 as cv
 import argparse
 
+
+def steganography_encode(input_image, input_text, bit_plane):
+    # Check if the message can be encoded in the image
+    if(len(input_text) > (input_image.size // 8)):
+        raise ValueError("Insufficient bytes")
+
+    # Get the bits of the input text
+    input_ascii = np.frombuffer(input_text.encode(), 'S1').view(np.uint8)
+    input_bits = np.unpackbits(input_ascii)
+
+    # Flatten input image array
+    output_flat = input_image.ravel()
+    output_use = output_flat[:len(input_bits)]
+
+    # You can set [0,1]:
+    # 1: x = x | pos
+    # 0: x = x & ~pos
+    mask_one = 1 << bit_plane
+    mask_zero = ~mask_one
+
+    output_flat[:len(input_bits)] = np.where(
+        input_bits, output_use | mask_one, output_use & mask_zero)
+
+    # Recover image shape
+    output_image = output_flat.reshape(input_image.shape)
+    return output_image
+
+
 def main():
-    print("aaa")
+
+    # Add arguments to encode steganography program
+    parser = argparse.ArgumentParser(description='Steganography Encoder.')
+    parser.add_argument('in_image', help='Name of the input image.')
+    parser.add_argument('in_text', help='Name of the input text.')
+    parser.add_argument('out_image', help='Name of the encoded image.')
+    parser.add_argument('bit_plane', help='Bit plane to hide msg.', type=int)
+    args = parser.parse_args()
+
+    # Read input image file
+    input_image = cv.imread(args.in_image, cv.IMREAD_COLOR)
+
+    # Read input text file
+    input_text = ''
+    with open(args.in_text, 'r') as file:
+        input_text = file.read() + chr(0)
+
+    output_image = steganography_encode(
+        input_image, input_text, args.bit_plane)
+    cv.imwrite(args.out_image, output_image)
+
 
 if __name__ == "__main__":
     main()
