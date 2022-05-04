87d6573422c188160a875958e1f68be9d45bc1c3
Percy Maldonado <maldonadoq.percy@gmail.com>
2022-05-04 20:30:24 -0300

    run all ways for each images

---
+++ exercise/ex2/execute.sh
@@ -0,0 +1,16 @@
+python3 halftones.py images/monalisa.png all;
+python3 halftones.py images/monalisa.png all --zigzag;
+python3 halftones.py images/monalisa.png all --color;
+python3 halftones.py images/monalisa.png all --color --zigzag;
+python3 halftones.py images/baboon.png all;
+python3 halftones.py images/baboon.png all --zigzag;
+python3 halftones.py images/baboon.png all --color;
+python3 halftones.py images/baboon.png all --color --zigzag;
+python3 halftones.py images/peppers.png all;
+python3 halftones.py images/peppers.png all --zigzag;
+python3 halftones.py images/peppers.png all --color;
+python3 halftones.py images/peppers.png all --color --zigzag;
+python3 halftones.py images/watch.png all;
+python3 halftones.py images/watch.png all --zigzag;
+python3 halftones.py images/watch.png all --color;
+python3 halftones.py images/watch.png all --color --zigzag
