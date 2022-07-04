6fc4aee0ef0502e9c4d229765138482a0cda9c3b
Percy Maldonado <maldonadoq.percy@gmail.com>
2022-07-04 14:10:52 -0300

    fix execute

--- exercise/ex4/execute.sh
+++ exercise/ex4/execute.sh
@@ -3,129 +3,129 @@ python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_rotation_near
 python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_scale_nearest.png -s 0.75,0.75 -m nearest
 python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_translation_nearest.png -t 50,50 -m nearest
 python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_reflection_nearest.png -r 1,-1 -m nearest
-python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_shear_nearest.png -k 0.25,0.25-m nearest
+python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_shear_nearest.png -k 0.25,0.25 -m nearest
 python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_rotation_bilinear.png -a 15 -m bilinear
 python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_scale_bilinear.png -s 0.75,0.75 -m bilinear
 python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_translation_bilinear.png -t 50,50 -m bilinear
 python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_reflection_bilinear.png -r 1,-1 -m bilinear
-python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_shear_bilinear.png -k 0.25,0.25-m bilinear
+python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_shear_bilinear.png -k 0.25,0.25 -m bilinear
 python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_rotation_bicubic.png -a 15 -m bicubic
 python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_scale_bicubic.png -s 0.75,0.75 -m bicubic
 python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_translation_bicubic.png -t 50,50 -m bicubic
 python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_reflection_bicubic.png -r 1,-1 -m bicubic
-python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_shear_bicubic.png -k 0.25,0.25-m bicubic
+python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_shear_bicubic.png -k 0.25,0.25 -m bicubic
 python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_rotation_lagrange.png -a 15 -m lagrange
 python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_scale_lagrange.png -s 0.75,0.75 -m lagrange
 python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_translation_lagrange.png -t 50,50 -m lagrange
 python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_reflection_lagrange.png -r 1,-1 -m lagrange
-python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_shear_lagrange.png -k 0.25,0.25-m lagrange
+python3 interpolation.py -i ./images/baboon.png -o ./images/baboon_shear_lagrange.png -k 0.25,0.25 -m lagrange
 
 echo butterfly
 python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_rotation_nearest.png -a 15 -m nearest
 python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_scale_nearest.png -s 0.75,0.75 -m nearest
 python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_translation_nearest.png -t 50,50 -m nearest
 python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_reflection_nearest.png -r 1,-1 -m nearest
-python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_shear_nearest.png -k 0.25,0.25-m nearest
+python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_shear_nearest.png -k 0.25,0.25 -m nearest
 python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_rotation_bilinear.png -a 15 -m bilinear
 python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_scale_bilinear.png -s 0.75,0.75 -m bilinear
 python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_translation_bilinear.png -t 50,50 -m bilinear
 python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_reflection_bilinear.png -r 1,-1 -m bilinear
-python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_shear_bilinear.png -k 0.25,0.25-m bilinear
+python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_shear_bilinear.png -k 0.25,0.25 -m bilinear
 python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_rotation_bicubic.png -a 15 -m bicubic
 python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_scale_bicubic.png -s 0.75,0.75 -m bicubic
 python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_translation_bicubic.png -t 50,50 -m bicubic
 python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_reflection_bicubic.png -r 1,-1 -m bicubic
-python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_shear_bicubic.png -k 0.25,0.25-m bicubic
+python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_shear_bicubic.png -k 0.25,0.25 -m bicubic
 python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_rotation_lagrange.png -a 15 -m lagrange
 python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_scale_lagrange.png -s 0.75,0.75 -m lagrange
 python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_translation_lagrange.png -t 50,50 -m lagrange
 python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_reflection_lagrange.png -r 1,-1 -m lagrange
-python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_shear_lagrange.png -k 0.25,0.25-m lagrange
+python3 interpolation.py -i ./images/butterfly.png -o ./images/butterfly_shear_lagrange.png -k 0.25,0.25 -m lagrange
 
 echo city
 python3 interpolation.py -i ./images/city.png -o ./images/city_rotation_nearest.png -a 15 -m nearest
 python3 interpolation.py -i ./images/city.png -o ./images/city_scale_nearest.png -s 0.75,0.75 -m nearest
 python3 interpolation.py -i ./images/city.png -o ./images/city_translation_nearest.png -t 50,50 -m nearest
 python3 interpolation.py -i ./images/city.png -o ./images/city_reflection_nearest.png -r 1,-1 -m nearest
-python3 interpolation.py -i ./images/city.png -o ./images/city_shear_nearest.png -k 0.25,0.25-m nearest
+python3 interpolation.py -i ./images/city.png -o ./images/city_shear_nearest.png -k 0.25,0.25 -m nearest
 python3 interpolation.py -i ./images/city.png -o ./images/city_rotation_bilinear.png -a 15 -m bilinear
 python3 interpolation.py -i ./images/city.png -o ./images/city_scale_bilinear.png -s 0.75,0.75 -m bilinear
 python3 interpolation.py -i ./images/city.png -o ./images/city_translation_bilinear.png -t 50,50 -m bilinear
 python3 interpolation.py -i ./images/city.png -o ./images/city_reflection_bilinear.png -r 1,-1 -m bilinear
-python3 interpolation.py -i ./images/city.png -o ./images/city_shear_bilinear.png -k 0.25,0.25-m bilinear
+python3 interpolation.py -i ./images/city.png -o ./images/city_shear_bilinear.png -k 0.25,0.25 -m bilinear
 python3 interpolation.py -i ./images/city.png -o ./images/city_rotation_bicubic.png -a 15 -m bicubic
 python3 interpolation.py -i ./images/city.png -o ./images/city_scale_bicubic.png -s 0.75,0.75 -m bicubic
 python3 interpolation.py -i ./images/city.png -o ./images/city_translation_bicubic.png -t 50,50 -m bicubic
 python3 interpolation.py -i ./images/city.png -o ./images/city_reflection_bicubic.png -r 1,-1 -m bicubic
-python3 interpolation.py -i ./images/city.png -o ./images/city_shear_bicubic.png -k 0.25,0.25-m bicubic
+python3 interpolation.py -i ./images/city.png -o ./images/city_shear_bicubic.png -k 0.25,0.25 -m bicubic
 python3 interpolation.py -i ./images/city.png -o ./images/city_rotation_lagrange.png -a 15 -m lagrange
 python3 interpolation.py -i ./images/city.png -o ./images/city_scale_lagrange.png -s 0.75,0.75 -m lagrange
 python3 interpolation.py -i ./images/city.png -o ./images/city_translation_lagrange.png -t 50,50 -m lagrange
 python3 interpolation.py -i ./images/city.png -o ./images/city_reflection_lagrange.png -r 1,-1 -m lagrange
-python3 interpolation.py -i ./images/city.png -o ./images/city_shear_lagrange.png -k 0.25,0.25-m lagrange
+python3 interpolation.py -i ./images/city.png -o ./images/city_shear_lagrange.png -k 0.25,0.25 -m lagrange
 
 echo house
 python3 interpolation.py -i ./images/house.png -o ./images/house_rotation_nearest.png -a 15 -m nearest
 python3 interpolation.py -i ./images/house.png -o ./images/house_scale_nearest.png -s 0.75,0.75 -m nearest
 python3 interpolation.py -i ./images/house.png -o ./images/house_translation_nearest.png -t 50,50 -m nearest
 python3 interpolation.py -i ./images/house.png -o ./images/house_reflection_nearest.png -r 1,-1 -m nearest
-python3 interpolation.py -i ./images/house.png -o ./images/house_shear_nearest.png -k 0.25,0.25-m nearest
+python3 interpolation.py -i ./images/house.png -o ./images/house_shear_nearest.png -k 0.25,0.25 -m nearest
 python3 interpolation.py -i ./images/house.png -o ./images/house_rotation_bilinear.png -a 15 -m bilinear
 python3 interpolation.py -i ./images/house.png -o ./images/house_scale_bilinear.png -s 0.75,0.75 -m bilinear
 python3 interpolation.py -i ./images/house.png -o ./images/house_translation_bilinear.png -t 50,50 -m bilinear
 python3 interpolation.py -i ./images/house.png -o ./images/house_reflection_bilinear.png -r 1,-1 -m bilinear
-python3 interpolation.py -i ./images/house.png -o ./images/house_shear_bilinear.png -k 0.25,0.25-m bilinear
+python3 interpolation.py -i ./images/house.png -o ./images/house_shear_bilinear.png -k 0.25,0.25 -m bilinear
 python3 interpolation.py -i ./images/house.png -o ./images/house_rotation_bicubic.png -a 15 -m bicubic
 python3 interpolation.py -i ./images/house.png -o ./images/house_scale_bicubic.png -s 0.75,0.75 -m bicubic
 python3 interpolation.py -i ./images/house.png -o ./images/house_translation_bicubic.png -t 50,50 -m bicubic
 python3 interpolation.py -i ./images/house.png -o ./images/house_reflection_bicubic.png -r 1,-1 -m bicubic
-python3 interpolation.py -i ./images/house.png -o ./images/house_shear_bicubic.png -k 0.25,0.25-m bicubic
+python3 interpolation.py -i ./images/house.png -o ./images/house_shear_bicubic.png -k 0.25,0.25 -m bicubic
 python3 interpolation.py -i ./images/house.png -o ./images/house_rotation_lagrange.png -a 15 -m lagrange
 python3 interpolation.py -i ./images/house.png -o ./images/house_scale_lagrange.png -s 0.75,0.75 -m lagrange
 python3 interpolation.py -i ./images/house.png -o ./images/house_translation_lagrange.png -t 50,50 -m lagrange
 python3 interpolation.py -i ./images/house.png -o ./images/house_reflection_lagrange.png -r 1,-1 -m lagrange
-python3 interpolation.py -i ./images/house.png -o ./images/house_shear_lagrange.png -k 0.25,0.25-m lagrange
+python3 interpolation.py -i ./images/house.png -o ./images/house_shear_lagrange.png -k 0.25,0.25 -m lagrange
 
 echo seagull
 python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_rotation_nearest.png -a 15 -m nearest
 python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_scale_nearest.png -s 0.75,0.75 -m nearest
 python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_translation_nearest.png -t 50,50 -m nearest
 python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_reflection_nearest.png -r 1,-1 -m nearest
-python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_shear_nearest.png -k 0.25,0.25-m nearest
+python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_shear_nearest.png -k 0.25,0.25 -m nearest
 python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_rotation_bilinear.png -a 15 -m bilinear
 python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_scale_bilinear.png -s 0.75,0.75 -m bilinear
 python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_translation_bilinear.png -t 50,50 -m bilinear
 python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_reflection_bilinear.png -r 1,-1 -m bilinear
-python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_shear_bilinear.png -k 0.25,0.25-m bilinear
+python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_shear_bilinear.png -k 0.25,0.25 -m bilinear
 python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_rotation_bicubic.png -a 15 -m bicubic
 python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_scale_bicubic.png -s 0.75,0.75 -m bicubic
 python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_translation_bicubic.png -t 50,50 -m bicubic
 python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_reflection_bicubic.png -r 1,-1 -m bicubic
-python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_shear_bicubic.png -k 0.25,0.25-m bicubic
+python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_shear_bicubic.png -k 0.25,0.25 -m bicubic
 python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_rotation_lagrange.png -a 15 -m lagrange
 python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_scale_lagrange.png -s 0.75,0.75 -m lagrange
 python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_translation_lagrange.png -t 50,50 -m lagrange
 python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_reflection_lagrange.png -r 1,-1 -m lagrange
-python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_shear_lagrange.png -k 0.25,0.25-m lagrange
+python3 interpolation.py -i ./images/seagull.png -o ./images/seagull_shear_lagrange.png -k 0.25,0.25 -m lagrange
 
 echo trip
 python3 interpolation.py -i ./images/trip.png -o ./images/trip_rotation_nearest.png -a 15 -m nearest
 python3 interpolation.py -i ./images/trip.png -o ./images/trip_scale_nearest.png -s 0.75,0.75 -m nearest
 python3 interpolation.py -i ./images/trip.png -o ./images/trip_translation_nearest.png -t 50,50 -m nearest
 python3 interpolation.py -i ./images/trip.png -o ./images/trip_reflection_nearest.png -r 1,-1 -m nearest
-python3 interpolation.py -i ./images/trip.png -o ./images/trip_shear_nearest.png -k 0.25,0.25-m nearest
+python3 interpolation.py -i ./images/trip.png -o ./images/trip_shear_nearest.png -k 0.25,0.25 -m nearest
 python3 interpolation.py -i ./images/trip.png -o ./images/trip_rotation_bilinear.png -a 15 -m bilinear
 python3 interpolation.py -i ./images/trip.png -o ./images/trip_scale_bilinear.png -s 0.75,0.75 -m bilinear
 python3 interpolation.py -i ./images/trip.png -o ./images/trip_translation_bilinear.png -t 50,50 -m bilinear
 python3 interpolation.py -i ./images/trip.png -o ./images/trip_reflection_bilinear.png -r 1,-1 -m bilinear
-python3 interpolation.py -i ./images/trip.png -o ./images/trip_shear_bilinear.png -k 0.25,0.25-m bilinear
+python3 interpolation.py -i ./images/trip.png -o ./images/trip_shear_bilinear.png -k 0.25,0.25 -m bilinear
 python3 interpolation.py -i ./images/trip.png -o ./images/trip_rotation_bicubic.png -a 15 -m bicubic
 python3 interpolation.py -i ./images/trip.png -o ./images/trip_scale_bicubic.png -s 0.75,0.75 -m bicubic
 python3 interpolation.py -i ./images/trip.png -o ./images/trip_translation_bicubic.png -t 50,50 -m bicubic
 python3 interpolation.py -i ./images/trip.png -o ./images/trip_reflection_bicubic.png -r 1,-1 -m bicubic
-python3 interpolation.py -i ./images/trip.png -o ./images/trip_shear_bicubic.png -k 0.25,0.25-m bicubic
+python3 interpolation.py -i ./images/trip.png -o ./images/trip_shear_bicubic.png -k 0.25,0.25 -m bicubic
 python3 interpolation.py -i ./images/trip.png -o ./images/trip_rotation_lagrange.png -a 15 -m lagrange
 python3 interpolation.py -i ./images/trip.png -o ./images/trip_scale_lagrange.png -s 0.75,0.75 -m lagrange
 python3 interpolation.py -i ./images/trip.png -o ./images/trip_translation_lagrange.png -t 50,50 -m lagrange
 python3 interpolation.py -i ./images/trip.png -o ./images/trip_reflection_lagrange.png -r 1,-1 -m lagrange
-python3 interpolation.py -i ./images/trip.png -o ./images/trip_shear_lagrange.png -k 0.25,0.25-m lagrange
+python3 interpolation.py -i ./images/trip.png -o ./images/trip_shear_lagrange.png -k 0.25,0.25 -m lagrange
